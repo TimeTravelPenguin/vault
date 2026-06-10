98099cf0a19b125590c83791966cee70bdf15d52
Phillip Smith <35803538+TimeTravelPenguin@users.noreply.github.com>
2026-06-10 19:49:33 +1000

    refactor!: remove support for connection strings to reduce complexity

--- crates/vault-app/examples/app_config.rs
+++ crates/vault-app/examples/app_config.rs
@@ -1,46 +1,25 @@
-use vault_app::{
-    AppError,
-    config::{AppConfig, from_file_path, try_from_connection_string},
-};
+use vault_app::{AppError, config::AppConfig};
 
 fn main() -> Result<(), AppError> {
     color_eyre::install()?;
 
-    let config_fp = AppConfig::new(from_file_path("db.sqlite"));
+    let config = AppConfig::new("db.sqlite");
 
-    let config_cs = AppConfig::new(
-        try_from_connection_string("sqlite://db.sqlite?mode=rwc")
-            .expect("Failed to parse database connection string"),
-    );
+    println!("Config {:#?}", config);
 
-    println!("Config (with db filepath): {:#?}", config_fp);
-    println!("\nConfig (with db connection string): {:#?}", config_cs);
-
-    let config_fp_json =
-        serde_json::to_string_pretty(&config_fp).expect("Failed to serialize config to JSON");
-    let config_cs_json =
-        serde_json::to_string_pretty(&config_cs).expect("Failed to serialize config to JSON");
+    let config_json =
+        serde_json::to_string_pretty(&config).expect("Failed to serialize config to JSON");
 
     println!("\n=== Serialisation ===");
-    println!("\nConfig (with db filepath) as JSON:\n{}", config_fp_json);
-    println!(
-        "\nConfig (with db connection string) as JSON:\n{}",
-        config_cs_json
-    );
+    println!("\nConfig (with db filepath) as JSON:\n{}", config_json);
 
     let deserialized_config_fp: AppConfig =
-        serde_json::from_str(&config_fp_json).expect("Failed to deserialize config from JSON");
-    let deserialized_config_cs: AppConfig =
-        serde_json::from_str(&config_cs_json).expect("Failed to deserialize config from JSON");
+        serde_json::from_str(&config_json).expect("Failed to deserialize config from JSON");
 
     println!("\n=== Deserialisation ===");
     println!(
         "\nDeserialized Config (with db filepath): {:#?}",
         deserialized_config_fp
-    );
-    println!(
-        "\nDeserialized Config (with db connection string): {:#?}",
-        deserialized_config_cs
     );
 
     Ok(())
--- crates/vault-app/src/config/mod.rs
+++ crates/vault-app/src/config/mod.rs
@@ -7,7 +7,6 @@ mod config_version;
 mod db_location;
 
 pub use config_version::ConfigVersion;
-pub use db_location::{DatabaseLocation, from_file_path, try_from_connection_string};
 
 pub const APP_NAME: &str = "vault-app";
 pub const CONFIG_FILE_NAME: &str = "config";
@@ -29,7 +28,7 @@ pub enum ConfigError {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppConfig {
     pub version: ConfigVersion,
-    pub database: DatabaseLocation,
+    pub database: PathBuf,
 }
 
 impl Default for AppConfig {
@@ -39,15 +38,15 @@ impl Default for AppConfig {
             .and_then(|path| path.parent().map(|parent| parent.to_path_buf()))
             .unwrap_or_else(|| ".".into());
 
-        Self::new(from_file_path(config_dir.join("db.sqlite")))
+        Self::new(config_dir.join("db.sqlite"))
     }
 }
 
 impl AppConfig {
-    pub fn new(db_location: DatabaseLocation) -> Self {
+    pub fn new(database: impl Into<PathBuf>) -> Self {
         Self {
             version: ConfigVersion::CURRENT,
-            database: db_location,
+            database: database.into(),
         }
     }
 }
